a95d2255aa07009ea8828cc2ebd46f2bc5e7bf2c
Slobodan Dan <slobodan.s.dan@gmail.com>
2024-05-21 20:21:44 +0200

    Fixed formating of guests.

--- app/guests/page.tsx
+++ app/guests/page.tsx
@@ -17,23 +17,31 @@ export default async function Page() {
     });
     const guests: GetGuestsResponse[] = await response.json();
 
+    let count = 0;
+    const guestGroups = guests.reduce((acc, { name, guests }) => {
+        const group = (
+            <div key={name}>
+                <div>
+                    <span>{++count}.</span> {name}
+                </div>
+                {guests.map((guest) => {
+                    return (
+                        <div key={guest.name} className='flex gap-x-2'>
+                            <div>{++count}.</div>
+                            <div>{guest.name}</div>
+                            {guest.age && <div>{` (${guest.age})`}</div>}
+                        </div>
+                    );
+                })}
+            </div>
+        );
+
+        return [...acc, group];
+    }, [] as JSX.Element[]);
+
     return (
         <main className='flex justify-center font-serif text-2xl text-amber-300 px-4 my-8'>
-            <div className='flex flex-col gap-y-2'>
-                {guests.map(({ name, guests }) => (
-                    <div key={name}>
-                        <div>{name}</div>
-                        {guests.map((guest) => {
-                            return (
-                                <div key={guest.name} className='flex gap-x-2'>
-                                    <div>{guest.name}</div>
-                                    {guest.age && <div>{guest.age} god.</div>}
-                                </div>
-                            );
-                        })}
-                    </div>
-                ))}
-            </div>
+            <div className='flex flex-col gap-y-6'>{guestGroups}</div>
         </main>
     );
 }
